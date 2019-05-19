7c9e933e7a0e80096b2044e1a06304a923d2fa9f
myTerminal <team.terminal@gmail.com>
2019-05-19 17:13:56 -0500

    better express sw.js

--- react/src/client/sw.js
+++ react/src/client/sw.js
@@ -1,65 +1,92 @@
 /* global caches fetch skipWaiting */
 
-var cacheName = '#sw-cache-string#',
+const cacheName = '#sw-cache-string#',
     origin = '#sw-origin#';
 
-this.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(
-                [
-                    origin,
-                    origin + 'index.html',
-                    origin + 'styles/vendor/ample-alerts/build/css/ample-alerts.dark.css',
-                    origin + 'styles/vendor/bootstrap/css/bootstrap.min.css',
-                    origin + 'styles/vendor/font-awesome/css/all.min.css',
-                    origin + 'styles/vendor/font-awesome/webfonts/fa-solid-900.woff2',
-                    origin + 'styles/styles.css',
-                    origin + 'scripts/scripts.js',
-                    origin + 'images/pencils.jpg',
-                    origin + 'fonts/OpenSans-Regular.ttf',
-                    origin + 'fonts/OpenSans-Bold.ttf',
-                    origin + 'icons/launcher-icon-1x.png',
-                    origin + 'icons/launcher-icon-2x.png',
-                    origin + 'icons/launcher-icon-4x.png',
-                    origin + 'manifest.json',
-                    origin + 'favicon.ico'
-                ]
-            );
-        }).catch(function (err) {
-            console.log(err);
-        })
-    );
-});
+this.addEventListener(
+    'install',
+    event => {
+        event.waitUntil(
+            caches.open(cacheName)
+                .then(
+                    cache => cache.addAll(
+                        [
+                            origin,
+                            origin + 'index.html',
+                            origin + 'styles/vendor/ample-alerts/build/css/ample-alerts.dark.css',
+                            origin + 'styles/vendor/bootstrap/css/bootstrap.min.css',
+                            origin + 'styles/vendor/font-awesome/css/all.min.css',
+                            origin + 'styles/vendor/font-awesome/webfonts/fa-solid-900.woff2',
+                            origin + 'styles/styles.css',
+                            origin + 'scripts/scripts.js',
+                            origin + 'images/pencils.jpg',
+                            origin + 'fonts/OpenSans-Regular.ttf',
+                            origin + 'fonts/OpenSans-Bold.ttf',
+                            origin + 'icons/launcher-icon-1x.png',
+                            origin + 'icons/launcher-icon-2x.png',
+                            origin + 'icons/launcher-icon-4x.png',
+                            origin + 'manifest.json',
+                            origin + 'favicon.ico'
+                        ]
+                    )
+                )
+                .catch(
+                    err => {
+                        console.log(err);
+                    }
+                )
+        );
+    });
 
-this.addEventListener('fetch', function (event) {
-    var urlWithoutQueryParams = event.request.url.split('?')[0];
+this.addEventListener(
+    'fetch',
+    event => {
+        const urlWithoutQueryParams = event.request.url.split('?')[0];
 
-    event.respondWith(caches.match(urlWithoutQueryParams).then(function (response) {
-        if (response) {
-            return response;
-        }
+        event.respondWith(
+            caches.match(urlWithoutQueryParams)
+                .then(
+                    response => {
+                        if (response) {
+                            return response;
+                        }
 
-        return fetch(event.request);
-    }));
-});
-
-this.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(keyList => Promise.all(keyList.map(key => {
-            if (key !== cacheName) {
-                return caches.delete(key);
-            } else {
-                return null;
-            }
-        })))
-    );
-});
-
-this.addEventListener('message', messageEvent => {
-    if (messageEvent.data === 'skipWaiting') {
-        return skipWaiting();
-    } else {
-        return null;
+                        return fetch(event.request);
+                    }
+                )
+        );
     }
-});
+);
+
+this.addEventListener(
+    'activate',
+    event => {
+        event.waitUntil(
+            caches.keys()
+                .then(
+                    keyList => Promise.all(
+                        keyList.map(
+                            key => {
+                                if (key !== cacheName) {
+                                    return caches.delete(key);
+                                } else {
+                                    return null;
+                                }
+                            }
+                        )
+                    )
+                )
+        );
+    }
+);
+
+this.addEventListener(
+    'message',
+    messageEvent => {
+        if (messageEvent.data === 'skipWaiting') {
+            return skipWaiting();
+        } else {
+            return null;
+        }
+    }
+);
