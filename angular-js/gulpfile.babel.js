b7fdc01d3a0e74c2ac479ac81660cfe74fb3d1c0
myTerminal <team.terminal@gmail.com>
2019-08-10 00:48:39 -0500

    tidy up gulpfile.babel.js

--- angular-js/gulpfile.babel.js
+++ angular-js/gulpfile.babel.js
@@ -19,57 +19,57 @@ const gulp = require('gulp'),
     buffer = require('vinyl-buffer'),
     browserify = require('browserify');
 
-const clean = (done) =>
+const clean = done =>
     del([outputDir], done);
 
 const copyBootstrapCss = () =>
     gulp.src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/bootstrap/css', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/bootstrap/css`, {
         prefix: 4
     }));
 
 const copyBootstrapFonts = () =>
     gulp.src([
         'node_modules/bootstrap/dist/fonts/**/*'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/bootstrap/fonts', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/bootstrap/fonts`, {
         prefix: 4
     }));
 
 const copyFontAwesomeCss = () =>
     gulp.src([
         'node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/font-awesome/css', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/font-awesome/css`, {
         prefix: 4
     }));
 
 const copyFontAwesomeFonts = () =>
     gulp.src([
         'node_modules/@fortawesome/fontawesome-free/webfonts/**/*'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/font-awesome/webfonts', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/font-awesome/webfonts`, {
         prefix: 4
     }));
 
 const copyAmpleAlertsCss = () =>
     gulp.src([
         'node_modules/ample-alerts/build/styles/**/*'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/ample-alerts/css', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/ample-alerts/css`, {
         prefix: 4
     }));
 
 const copyAmpleAlertsFonts = () =>
     gulp.src([
         'node_modules/ample-alerts/build/fonts/**/*'
-    ]).pipe(gulpCopy(outputDir + '/styles/vendor/ample-alerts/fonts', {
+    ]).pipe(gulpCopy(`${outputDir}/styles/vendor/ample-alerts/fonts`, {
         prefix: 4
     }));
 
 const copyOthers = () =>
     gulp.src([
-        sourceDir + '/fonts/**/*',
-        sourceDir + '/images/**/*',
-        sourceDir + '/icons/**/*',
-        sourceDir + '/favicon.ico'
+        `${sourceDir}/fonts/**/*`,
+        `${sourceDir}/images/**/*`,
+        `${sourceDir}/icons/**/*`,
+        `${sourceDir}/favicon.ico`
     ]).pipe(gulpCopy(outputDir, {
         prefix: 2
     }));
@@ -85,16 +85,16 @@ const copy = gulp.parallel(
 );
 
 const templates = () =>
-    gulp.src(sourceDir + '/scripts/templates/*.html')
+    gulp.src(`${sourceDir}/scripts/templates/*.html`)
         .pipe(gulpAngularTemplateCache({
             root: 'scripts/templates',
             module: 'templateWeb'
         }))
-        .pipe(gulp.dest(outputDir + '/scripts'));
+        .pipe(gulp.dest(`${outputDir}/scripts`));
 
 const manifestDebug = () =>
     gulp.src([
-        sourceDir + '/manifest.json'
+        `${sourceDir}/manifest.json`
     ]).pipe(gulpThat(function (input) {
         return input
             .replace(/#manifest-origin#/g, '/');
@@ -102,72 +102,72 @@ const manifestDebug = () =>
 
 const manifest = () =>
     gulp.src([
-        sourceDir + '/manifest.json'
-    ]).pipe(gulpThat(function (input) {
+        `${sourceDir}/manifest.json`
+    ]).pipe(gulpThat(input => {
         return input
             .replace(/#manifest-origin#/g, configs.origin);
     })).pipe(gulp.dest(outputDir));
 
 const styles = () =>
-    gulp.src(sourceDir + '/styles/**/*.less')
+    gulp.src(`${sourceDir}/styles/**/*.less`)
         .pipe(gulpLess())
         .pipe(gulpConcat('styles.css'))
         .pipe(gulpCleanCss())
-        .pipe(gulp.dest(outputDir + '/styles'));
+        .pipe(gulp.dest(`${outputDir}/styles`));
 
 const scriptsDebug = () =>
     browserify({
-        entries: sourceDir + '/scripts/app.js',
+        entries: `${sourceDir}/scripts/app.js`,
         debug: true
     }).transform('babelify')
         .bundle()
         .pipe(source('scripts.js'))
         .pipe(buffer())
-        .pipe(gulp.dest(outputDir + '/scripts/'));
+        .pipe(gulp.dest(`${outputDir}/scripts/`));
 
 const scripts = () =>
     browserify({
-        entries: sourceDir + '/scripts/app.js'
+        entries: `${sourceDir}/scripts/app.js`
     }).transform('babelify')
         .bundle()
         .pipe(source('scripts.js'))
         .pipe(buffer())
         .pipe(gulpUglify())
-        .pipe(gulp.dest(outputDir + '/scripts/'));
+        .pipe(gulp.dest(`${outputDir}/scripts/`));
 
 const htmlDebug = () =>
     gulp.src([
-        sourceDir + '/*.html'
+        `${sourceDir}/*.html`
     ]).pipe(gulpEjs({
         titlePrefix: '[DEBUG] ',
         baseUrl: '/'
-    })).pipe(gulpThat(function (input) {
+    })).pipe(gulpThat(input => {
         return input
             .replace(/#cache-buster-token#/g, '');
     })).pipe(gulp.dest(outputDir));
 
 const html = () =>
     gulp.src([
-        sourceDir + '/*.html'
+        `${sourceDir}/*.html`
     ]).pipe(gulpEjs({
         titlePrefix: '',
         baseUrl: configs.domain + configs.origin
-    })).pipe(gulpThat(function (input) {
+    })).pipe(gulpThat(input => {
         return input
             .replace(/#cache-buster-token#/g, (new Date()).getTime());
     })).pipe(gulp.dest(outputDir));
 
 const copyServiceWorker = () =>
     gulp.src([
-        sourceDir + '/sw.js'
-    ]).pipe(gulpThat(function (input) {
+        `${sourceDir}/sw.js`
+    ]).pipe(gulpThat(input => {
         return input
             .replace(/#sw-cache-string#/g, (new Date().getTime()))
             .replace(/#sw-origin#/g, configs.origin);
     })).pipe(gulp.dest(outputDir));
 
 const lint = () =>
-    gulp.src(sourceDir + '/scripts/**/*.js')
+    gulp.src(`${sourceDir}/scripts/**/*.js`)
         .pipe(gulpEslint())
         .pipe(gulpEslint.format())
         .pipe(gulpEslint.failAfterError());
@@ -197,14 +197,14 @@ const build = gulp.series(
 const develop = () => {
     gulp.watch(
         [
-            sourceDir + '/*.html'
+            `${sourceDir}/*.html`
         ],
         htmlDebug
     );
 
     gulp.watch(
         [
-            sourceDir + '/scripts/templates/**/*.html'
+            `${sourceDir}/scripts/templates/**/*.html`
         ],
         gulp.series([
             templates,
@@ -214,14 +214,14 @@ const develop = () => {
 
     gulp.watch(
         [
-            sourceDir + '/styles/**/*.less'
+            `${sourceDir}/styles/**/*.less`
         ],
         styles
     );
 
     gulp.watch(
         [
-            sourceDir + '/scripts/**/*.js'
+            `${sourceDir}/scripts/**/*.js`
         ],
         scriptsDebug
     );
@@ -232,6 +232,6 @@ export {
     build,
     develop,
     lint
-}
+};
 
 export default build;
